fa6369a5ff6b250bf5ef57b13c9f497e5f26b046
visiontechinfrasolution-hub <visiontechinfrasolution@gmail.com>
2026-04-24 18:24:29 +0530

    Update Visiontech_Portal.py

--- Visiontech_Portal.py
+++ Visiontech_Portal.py
@@ -174,7 +174,7 @@ if st.session_state.current_page == "Dashboard":
         if st.button("🏗️\nSite Detail"): navigate_to("Site")
         if st.button("📁\nData Entry"): navigate_to("Data")
         if st.button("📢\nRFAI Billing"): navigate_to("RFAI")
-        if st.button("📄\nJMS Generator"): navigate_to("JMS")
+        if st.button("📜\nVintage PDF"): navigate_to("PDFFormat")
        
 # --- PAGES LOGIC ---
 elif st.session_state.current_page != "Dashboard":
