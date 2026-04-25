873ef269efc7c5d00f2cd994e694414a54e537db
visiontechinfrasolution-hub <visiontechinfrasolution@gmail.com>
2026-04-25 09:49:09 +0530

    Update Visiontech_Portal.py

--- Visiontech_Portal.py
+++ Visiontech_Portal.py
@@ -171,7 +171,7 @@ if st.session_state.current_page == "Dashboard":
                
     with c3:
         if st.button("🏗️\nSite Detail"): navigate_to("Site")
-        if st.button("📁\nData Entry"): navigate_to("Data")
+        if st.button("📁\nData Entry"): st.switch_page("pages/data_entry.py")
         if st.button("📢\nRFAI Billing"): navigate_to("RFAI")
         if st.button("📜\nVintage PDF"): navigate_to("PDFFormat")
        
